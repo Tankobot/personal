795297be2f5f20d5a21e1cc41de3554818be7b07
Tankobot <tankobot@sbcglobal.net>
2014-11-01 15:01:30 -0700

    Update master.lua
    Added a clock function for sychronizing purposes.

--- bank/master.lua
+++ bank/master.lua
@@ -2,3 +2,11 @@
 Name: The Master 
 Version: 0.0 
 --]]
+
+--Create Clock 
+function clockIt(interval) 
+	alarmTime = 24000/os.time-(24000%os.time())
+	os.setAlarm(alarmTime+200)
+	event = os.pullEvent()
+	--TODO
+end
