ed10597c6f2103124cbe9ca6daf458154ed02fa4
Tankobot <tankobot@sbcglobal.net>
2014-11-13 22:01:43 -0800

    Update encode.lua
    Improved alphabet encryption environment system.

--- encryption/encode.lua
+++ encryption/encode.lua
@@ -6,17 +6,28 @@ Make sure your text only includes characters specified in the alphabet.
 --]]
 
 arg = {...}
+
+if #arg < 2 then
+  error(0,"Encode usage: encode <string> <key> [table]")
+end
+
 file = arg[1]
 
-alpha = [=[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890]=]
-function cutUp(oldString) 
-  local newString = ""
-  local newTable = {}
-  for i=1,string.len(oldString) do
-    newString = string.sub(oldString,i,i+1)
-    newTable[i] = newString
+if arg[3] ~= nil then 
+  alpha = arg[3] 
+else 
+  alpha = [=[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890]=]
+  function cutUp(oldString) 
+    local newString = ""
+    local newTable = {}
+    for i=1,string.len(oldString) do
+      newString = string.sub(oldString,i,i+1)
+      newTable[i] = newString
+    end
+    return newTable
   end
-  return newTable
 end
 
 alpha = cutUp(alpha)
+
+key = arg[2]
