85d95b02ff549ac130c554558979079217acc494
Tankobot <tankobot@users.noreply.github.com>
2014-12-23 10:27:22 -0800

    Added a small skeleton to the bank shell code.

--- bank/bank.lua
+++ bank/bank.lua
@@ -2,7 +2,24 @@
 This is the United Bank administrator shell. 
 --]]
 
---Checking the command 
+arg={...}
+
+--Check for the add command. 
 if arg[1] == "add" then 
+	
+end
+
+--Check for the sub command. 
+if arg[1] == "sub" then 
+	
+end
+
+--Check for the transfer command. (i.e. tran) 
+if arg[1] == "tran" then 
+	
+end
+
+--Check for the status command. 
+if arg[1] == "status" then 
 	
 end
