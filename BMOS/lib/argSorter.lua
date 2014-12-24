9bfcbc9bc7f8bc205410b07ea7691c27cb9416d1
Tankobot <tankobot@users.noreply.github.com>
2014-12-24 10:30:31 -0800

    Finished the first version of the argument sorting library.

--- BMOS/lib/argSorter.lua
+++ BMOS/lib/argSorter.lua
@@ -4,6 +4,19 @@ Title: Argument Sorting Function
 Author: Tankobot (Michael Bradley) 
 --]]
 
-local function argSort(args)
-	
+local function argSort(argA)
+	local argB = {}
+	local i=1
+	while i < #argA do 
+		if not string.fing("-",argA[i]) then 
+			argB[tostring(argA[i])]=argA[i+1]
+			i=i+3
+			j=j+1
+		else
+			argB[j]=argA[i]
+			i=i+1
+			j=j+1
+		end
+	end
+	return argB
 end
