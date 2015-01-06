7a558c67f0d4abdd34e7224e823be66e67b6b710
Tankobot <Tankobot@users.noreply.github.com>
2015-01-05 16:16:04 -0800

    Added a small portion to the eventHandler but havve not finished the control panel.

--- BMOS/tools/eventHandler.lua
+++ BMOS/tools/eventHandler.lua
@@ -3,3 +3,18 @@ Title: Computercraft Event Handler
 Author: Tankobot 
 Version: 0.1 
 --]]
+
+arg = {...}
+
+--Open program from argument as coroutine and specify it as the master coroutine. 
+masterPath = arg[1]
+masterFunc = loadfile(masterPath)
+masterCo = coroutine.create(masterFunc)
+
+--Starting master program. 
+eventType, extra = coroutine.resume(masterCo, "info", "master")
+if eventType == "admin" then 
+	eventType, extra = coroutine.resume(masterCo, false, "Not supported yet.")
+end
+
+--TODO
