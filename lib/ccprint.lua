353aed5478d4c93060cfd56388a14e416a475d14
Michael Bradley <tankobot@users.noreply.github.com>
2015-01-09 13:38:24 -0800

    Started to work on a window border drawing function.

--- lib/ccprint.lua
+++ lib/ccprint.lua
@@ -1,5 +1,22 @@
 --[[
-Title: Printer API for ComputerCraft. 
+Title: Window Drawing API
 Author: Tankobot (Michael Bradley) 
 --]] 
 
+--Window border function. 
+local function create(x,y,w,h,screen) 
+	bgColor = colors.white 
+	txtColor = colors.black 
+	bordColor = colors.black 
+	if not screen then 
+		screen = peripheral.wrap(screen)
+	else 
+		screen = term
+	end
+	obj = {
+		bgColor = bgColor,
+		txtColor = txtColor,
+		bordColor = bordColor,
+		isColor = screen.isColor()
+	)
+end
