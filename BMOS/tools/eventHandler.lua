dc26fab66736a182d36eaa49c344e5ca49010927
Tankobot <tankobot@users.noreply.github.com>
2015-01-10 01:33:40 -0800

    Made some major additions to the task manager and it's now in semi working order.

--- BMOS/tools/eventHandler.lua
+++ BMOS/tools/eventHandler.lua
@@ -1,20 +1,59 @@
 --[[
-Title: Computercraft Event Handler 
+Title: Computercraft Task Manager 
 Author: Tankobot 
 Version: 0.1 
 --]]
 
 arg = {...}
+assert(arg[1],"Usage: program <path>")
 
 --Open program from argument as coroutine and specify it as the master coroutine. 
-masterPath = arg[1]
-masterFunc = loadfile(masterPath)
-masterCo = coroutine.create(masterFunc)
+masterFunc = loadfile(arg[1])
 
---Starting master program. 
-eventType, extra = coroutine.resume(masterCo, "info", "master")
-if eventType == "admin" then 
-	eventType, extra = coroutine.resume(masterCo, false, "Not supported yet.")
+
+--Create the task table and add the master coroutine to it. 
+local task = {}
+task["master"] = coroutine.create(masterFunc)
+
+
+--Create admin table. 
+--[[ Allows the task manager to add a coroutine to the list of running coroutines. ]]-- 
+local admin = {}
+
+function admin.add(path, name)
+	local newFunc = loadfile(path)
+	task[name] = coroutine.create(newFunc)
 end
 
---TODO
+function admin.run(coro, ...)
+	local arg = {...} 
+	local result = {coroutine.resume(coro, unpack(arg))}
+	local dump = table.remove(result, 1)
+	local eventType = table.remove(result, 1)
+	return eventType, result
+end
+
+function admin.addR(path, name, ...)
+	admin.add(path, name)
+	local arg = {...}
+	return admin.run(task[name], unpack(arg))
+end
+
+function admin.exit()
+	error("Closing task manager...", 0)
+end
+
+
+--Starting master program. 
+eType, extra = admin.run(task.master, "info", "master")
+while true do
+	if eType == "admin" then --Checking for admin commands. 
+		admin[extra[1]](unpack(extra, 2))
+	end
+	if coroutine.status(task.master) == "dead" then 
+		error("Master thread is now dead.", 0)
+	else
+		event = {os.pullEvent()}
+	end
+	eType, extra = admin.run(task.master, unpack(event))
+end
