485c27bca00624f5e3d57923c3294ac067e9a36d
Tankobot <Tankobot@users.noreply.github.com>
2015-01-13 22:36:44 -0800

    Added the guts of the task manager API for ease of use.

---
+++ lib/tskMng.lua
@@ -0,0 +1,59 @@
+--[[
+Title: Task Manager API 
+Description: For ease of task manager functions when programming. 
+--]]
+
+local function current(newTask)
+	coroutine.yield("admin", "current", newTask)
+end
+
+local function listGet()
+	local info = coroutine.yield("admin", "listGet")
+	return info
+end
+
+local function garbage()
+	coroutine.yield("admin", "garbage")
+end
+
+local function add(func, name)
+	coroutine.yield("admin", "add", func, name)
+end
+
+local function run(coro, ...)
+	coroutine.yield("admin", "run", coro, ...)
+end
+
+local function prun(coro, ...)
+	coroutine.yield("admin", "prun", coro, ...)
+end
+
+local function addR(func, name, ...)
+	coroutine.yield("admin", "addR", func, name, ...)
+end
+
+nativeExit = exit
+
+local function exit()
+	coroutine.yield("admin", "exit")
+end
+
+local function event(...)
+	event = {...}
+end
+
+nativeLoadstring = loadstring 
+
+local function loadstring(block)
+	coroutine.yield("admin", "loadstring", block)
+end
+
+nativeError = error
+
+local function error(message)
+	coroutine.yield("admin", "error", message)
+end
+
+local function setCall(bool)
+	coroutine.yield("admin", "setCall", bool)
+end
