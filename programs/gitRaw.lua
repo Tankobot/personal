20e45b8c104f1b7400318b3efa144347fefa185f
Michael Bradley <tankobot@users.noreply.github.com>
2015-01-21 11:35:15 -0800

    Fixed

--- programs/gitRaw.lua
+++ programs/gitRaw.lua
@@ -14,9 +14,10 @@ saveLocation = arg[2]
 
 print("Starting to retrieve file from GitHub.")
 dump, raw = pcall(http.get, "https://raw.githubusercontent.com/"..repLocation)
-if raw == false then 
+if dump == false then 
 	error("Failed to retrieve file from remote server.")
 end
 file = fs.open(saveLocation, "w") 
-file.write(raw.readLine())
+file.write(raw.readAll())
 file.close()
+print("File Downloaded.")
