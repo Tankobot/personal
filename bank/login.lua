8a2cfdb1945a7c2a560b164ffbb07b1354852e2e
Tankobot <Tankobot@users.noreply.github.com>
2015-03-21 20:22:45 -0700

    Create login.lua

---
+++ bank/login.lua
@@ -0,0 +1,22 @@
+--Simple Program to Allow Tellers to Login. 
+
+while true do 
+	local start = nil
+	write("Username:")
+	local user = read()
+	write("Password:")
+	local pass = read()
+	check, file = pcall(fs.open, "/users/"..user)
+	if check == true then 
+		linePass = file.readLine()
+		if pass == linePass then 
+			func = loadfile(/users/"..user)
+			start = func()
+		else
+			print("Failed to login.")
+		end
+	else
+		print("Failed to login.")
+	end
+	pcall(start)
+end
