b02a12e687fc784748f2c0b004fceac097f786fa
Tankobot <Tankobot@users.noreply.github.com>
2015-03-21 22:09:32 -0700

    Update bank.lua

--- bank/bank.lua
+++ bank/bank.lua
@@ -17,18 +17,26 @@ for i=1,#apis do
 	assert(correct,"The library "..apis[i].." did not load correctly or does not exist.")
 end
 
+function nameCheck(name)
+	assert(fs.exists("users/"..name), "The user have specified does not exist.")
+end
+
 --Check for the add command. 
 if arg[1] == "add" then 
+	nameCheck(arg[2])
 	bank.add(arg)
 end
 
 --Check for the sub command. 
 if arg[1] == "sub" then 
+	nameCheck(arg[2])
 	bank.sub(arg)
 end
 
 --Check for the transfer command. (i.e. tran) 
 if arg[1] == "trans" then 
+	nameCheck(arg[2])
+	nameCheck(arg[3])
 	bank.trans(arg)
 end
 
