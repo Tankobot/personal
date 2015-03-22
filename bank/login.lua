5f06c6fc51b8586ec93854d465d42d63d1dd433b
Tankobot <Tankobot@users.noreply.github.com>
2015-03-22 00:11:09 -0700

    Update login.lua

--- bank/login.lua
+++ bank/login.lua
@@ -1,23 +1,51 @@
 --Simple Program to Allow Tellers to Login. 
 
 while true do 
+	term.clear()
+	term.setCursorPos(1,1)
+	print("//BankLogin//")
 	local start = nil
 	write("Username:")
 	local user = read()
 	write("Password:")
 	local pass = read("")
-	file = fs.open("/user/"..user, "r")
-	if file then 
-		linePass = file.readLine()
-		linePass = string.sub(linePass, string.find(linePass, "#")+1)
-		if pass == linePass then 
-			func = loadfile("/user/"..user)
-			start = func()
+	if user == "new" then 
+		print("Please right-click the sensor above the computer to sign in.")
+		local event, info = os.pullEvent("player")
+		if fs.exists("user/"..info) then 
+			print("Account already exists. Contact DLA for support.")
+		else
+			while not check do 
+				write("RePass:")
+				checkpass = read("")
+				if pass == checkpass then 
+					print("New account created under, "..info) 
+				else 
+					term.clear()
+					term.setCursorPos(1,1)
+					print("Passwords did not match.")
+				end
+			end
+			local file = fs.open("user/"..info, "w")
+			file.write([[-- #]]..newpass.."\nfunction func()\n"..
+				"dofile('perm/default',"..info..")\n"
+				"end")
+			file.close()
+		end
+	else
+		file = fs.open("/user/"..user, "r")
+		if file then 
+			linePass = file.readLine()
+			linePass = string.sub(linePass, string.find(linePass, "#")+1)
+			if pass == linePass then 
+				func = loadfile("/user/"..user)
+				start = func()
+			else
+				print("Failed to login.")
+			end
 		else
 			print("Failed to login.")
 		end
-	else
-		print("Failed to login.")
+		pcall(start)
 	end
-	pcall(start)
 end
