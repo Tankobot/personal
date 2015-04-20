afc8413357e58582655fba77d418d395a91b2385
Tankobot <Tankobot@noreply.github.com>
2015-04-20 13:28:51 -0700

    Add code to matrix create function. Setup entries.

--- lib/matrix.lua
+++ lib/matrix.lua
@@ -1,41 +1,28 @@
 --[[
 Title: Matrix Library 
-Reason: Lua does not natively support matrices without excessive programming. 
 Author: Tankobot (Michael Bradley) 
 --]] 
 
-local function bool(a, ...)
-	if a then
-		return true, bool(...)
-	elseif a == nil then
-		return false
-	end
-end
-
-local meta = {}
 local matrix = {}
-local mt = matrix
-local eval = luaL_dostring
+local version = "1.0.13"
 
-local function mt:create(dim) --Create matrix object. 
+local function matrix:create(dim) --Create matrix object. 
+	local obj = {}
+	local obj["index"] = {}
+	local obj["data"] = {}
+	local obj["type"] = "Matrix: "..version
+	local obj["dim"] = dim
+end
+
+local function matrix:getIndex(dim) --Retrieve index of all objects based on a certain dimension. 
 	--TODO
 end
 
-local function mt:getIndex(dim) --Retrieve index of all objects based on a certain dimension. 
-	--TODO
-end
-
-local function mt:index(entry, ...) --Add an object to the indexes of the matrix. 
+local function matrix:index(entry, ...) --Assign an object to the indexes of the matrix. 
 	local arg = {...}
-	local num
-	local function check(tab1, tab2, i)
-		
-	end
-	assert(#arg==#self.dim, "The number of indexes is not equal to the number of arguments.")
-	assert(not check(bool
-	--TODO
+	self[arg[1]]--TODO
 end
 
-local function mt:set(entry, ...) --Set data object in matrix. (...) must have #arguments equal to of dims. 
+local function matrix:set(entry, ...) --Set data object in matrix. (...) must have #arguments equal to of dims. 
 	--TODO
 end
