367db087984a7f27a7b7cb058eb869fce784378c
Tankobot <Tankobot@users.noreply.github.com>
2015-07-15 13:21:21 -0700

    My unfinished go at genetic algorithms, in Lua.

---
+++ ai/evolve.lua
@@ -0,0 +1,19 @@
+--Not a serious genetic algorithm 
+
+local args = {...}
+local popLim = args[1]
+local genLim = args[2]
+local targets = {unpack(args)}
+
+local population = {}
+
+for i=1, popLim do
+	population[i] = {}
+	for j=1, #targets do
+		
+	end
+end
+
+local function crossover(a, b)
+	
+end
