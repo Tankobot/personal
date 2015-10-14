033e710b356e535dc0c56e22d70d6993e4e67e27
Michael B. <Tankobot@users.noreply.github.com>
2015-10-14 11:30:58 -0700

    Update synthFactor.lua

--- school/synthFactor.lua
+++ school/synthFactor.lua
@@ -1,14 +1,24 @@
 local mid = {0}
-local resL = {1,5,3,8}
-local divisors = {1,2,4,8}
+local resN = {1,4,-3,-18}
+local divisors = {1,2,3,6,9,18}
+local resL = {}
+
+local c = #divisors
+for i=1, c do
+	divisors[#divisors+1]=-divisors[i]
+end
+
+print(table.unpack(divisors))
 
 local function factor(coeff, divisor, res)
+	print(divisor)
 	for i=1, #coeff-1 do
- 		mid[i+1] = res[i]
+ 		mid[i+1] = res[i]*divisor
 		res[i+1] = mid[i+1]+coeff[i+1]
 	end
 	if res[#res]==0 then
 		res[#res] = nil
+		print("Factored")
 		return true, res
 	else
 		return false
@@ -18,13 +28,17 @@ end
 local state
 local factoring = true
 while factoring do
-	resN = {table.unpack(resL)}
+	resL = {table.unpack(resN)}
+	mid = {0}
+	if #resL==1 then
+		break
+	end
 	for i=1, #divisors do
-		state, resN = factor(resL, divisors[i], resN)
 		print("###")
-		print(table.concat(resL,","))
-		print(table.concat(mid,","))
-		print(table.concat(resN,","))
+		state = factor(resL, divisors[i], resN)
+		print(table.unpack(resL))
+		print(table.unpack(mid))
+		print(table.unpack(resN))
 		if state then
 			break
 		end
