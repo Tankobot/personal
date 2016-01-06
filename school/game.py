cb5d4fea5ca9e52279aea18e72d5b08b2295ecce
Michael B <Tankobot@users.noreply.github.com>
2016-01-06 14:30:13 -0800

    Update game.py

--- school/game.py
+++ school/game.py
@@ -9,15 +9,26 @@ assert((difficulty <= 9) and (difficulty >= 1))
 pad = [[False for i in range(3)] for i in range(3)]
 
 def draw():
-  for i in pad:
-    for j in i:
-      
+  for x in pad:
+    for y in x:
+      if (x == player.loc[0]) and (y == player.loc[1]):
+        if pad[x][y]:
+          print('I', end='')
+        else:
+          print('Q', end='')
+      else:
+        print(pad[x][y], end='')
+    print()
 
 def pop(x, y):
   pad[x][y+1] = not pad[x][y+1]
   pad[x][y-1] = not pad[x][y-1]
   pad[x+1][y] = not pad[x+1][y]
   pad[x-1][y] = not pad[x-1][y]
+  
+class InputError(Exception):
+  def __init__(self, message):
+    self.message = message
 
 class player:
   self.loc = [0, 0]
@@ -29,7 +40,13 @@ class player:
     for i in cmds:
       x = self.loc[0]
       y = self.loc[1]
-      if i == 'd':
+      if i == 's':
+        self.loc[1] = y + 1
+      elif i == 'w':
+        self.loc[1] = y - 1
+      elif i == 'a':
         self.loc[0] = x + 1
-      elif i == :
-        
+      elif i == 'd':
+        self.loc[0] = x - 1
+      else
+        raise InputError()
